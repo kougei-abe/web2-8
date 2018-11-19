6ff31b8e641f0f9a777b8ac1e49d85db1f9807a5
kougei-abe <38421201+kougei-abe@users.noreply.github.com>
2018-11-19 16:55:42 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,3 +1,4 @@
+// カウントの処理
 var separate_time = function(time){
   var sec = time.getSeconds();
   var min = time.getMinutes();
@@ -25,6 +26,20 @@ var refresh = function(){
   setTimeout(update, 1000);
 }
 update();
+
+// ページ遷移の処理
+function getFileName(){
+  return window.location.href.split('/').pop();
+}
+
+var filename = getFileName();
+var opt;
+if(filename === 'other.html'){
+  opt = document.querySelector('option[value="other.html"]');
+}else{
+  opt = document.querySelector('option[value="index.html"]');
+}
+opt.selected = true;
 
 document.getElementById('form').select.onchange = function(){
     location.href = document.getElementById('form').select.value;
