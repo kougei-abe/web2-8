774dd45130335ba05fea685977fac1ff112dfa11
kougei-abe <38421201+kougei-abe@users.noreply.github.com>
2018-11-19 16:17:53 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -16,4 +16,9 @@ document.getElementById('countdown').textContent =
   counter[3] + '日' +
   counter[2] + '時' +
   counter[1] + '分' +
-  counter[0] + '秒'
+  counter[0] + '秒';
+
+var refresh = function(){
+  setTimeout(update, 1000);
+}
+update();
